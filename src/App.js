1e00965d4f3ec8198b111ec1f60ec4bc719f5178
Jazlyn Nguyen <jazlynnguyen@Jazlyns-MBP.home>
2020-10-18 04:15:34 -0400

    added component libraries

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React, { useState } from 'react';
+import 'antd/dist/antd.css' ;
+import { Button } from 'antd';
+import {CheckCircleTwoTone, CloseCircleTwoTone, FormOutlined, ScheduleTwoTone, SmileTwoTone, HeartTwoTone, ShoppingTwoTone} from '@ant-design/icons';
+import { Layout, Space } from 'antd';
+import { Input } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
+const { TextArea } = Input;
 
 function App() {
 
@@ -29,6 +37,21 @@ function App() {
    }
   };
 
+  // delete all items in list
+  const clearList = () => {
+    setTasks([]);
+  }
+
+  // order items by date
+  const orderDate = () => {
+
+  }
+
+  // order items by name
+  const orderName = () => {
+
+  }
+
   const ToDoItem = ({ task }) => {
 
     const [status, setStatus] = useState(false);
@@ -51,14 +74,17 @@ function App() {
     return (
       <div
       style ={{
-        border: "3px solid black",
+        border: "3px solid pink",
         width: "300px",
+        marginTop: "10px",
+        marginRight: "10px",
         marginBottom: "50px",
         padding: "10px",
         alignItems: "center",
         textAlign: "center",
         display: "flex",
         flexDirection: "column",
+        backgroundColor: "pink"
       }}>
 
       {status ? (
@@ -74,56 +100,138 @@ function App() {
         </div>
       )}
       
-      <button onClick={checkOff}> Check Off! </button>
-      <button onClick={deleteTask}>Delete!</button>
+      <Button type="default" shape="round" value="small" icon={<CheckCircleTwoTone twoToneColor="#b7eb8f"/>} onClick={checkOff}> check off </Button>
+      <Button type="default" shape="round" value= "small" icon={<CloseCircleTwoTone twoToneColor="#ffa39e" />}onClick={deleteTask}>delete</Button>
 
       </div>
     );
   };
     
   return(
+
+    <Layout>
+    
+    <Header style={{backgroundColor: "#d3adf7", fontSize: "26px"}}>
+      <div style={{
+        textAlign: "center",
+        alignItems: "center",
+        backgroundColor: "#d3adf7",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center"
+      }}>
+
+      <Space size = 'middle' direction = 'horizontal'>
+        <h1> your to do list </h1>
+        <FormOutlined/>
+      </Space>
   
-    <div
-    style={{
-      alignItems: "center", 
-      backgroundColor:'ivory', 
+
+      </div>
+    </Header>
+    
+    <Layout>
+      <Sider width="400" style={{backgroundColor: "#ffadd2"}}>
+        <div style={{
+          alignItems: "center",
+          textAlign: "center"
+        }}>
+
+          <br></br>
+          <Space size='small' direction='horizontal'>
+            <SmileTwoTone twoToneColor="#fff566" style={{fontSize: '18px'}}/>
+            <ScheduleTwoTone twoToneColor="#b7eb8f" style={{fontSize: '18px'}}/>
+            <ShoppingTwoTone twoToneColor="#d3adf7" style={{fontSize: '18px'}}/>
+            <HeartTwoTone twoToneColor="#ffadd2" style={{fontSize: '18px'}}/>
+          </Space>
+
+          <br></br>
+          <br></br>
+
+          <Input style={{width: 300}}
+            placeholder="name of task"
+            type="text" 
+            value = {taskName}
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <br></br>
+          <br></br>
+          <br></br>
+
+          <TextArea style={{width: 300}}
+            placeholder="description of task"
+            type="text"
+            value = {description}
+            onChange={(e) => setDescription(e.target.value)}
+            autoSize={{minRows: 3}}>
+            
+          </TextArea>
+      
+          <br></br>
+          <br></br>
+
+          <Input style={{width: 300}}
+            placeholder="due date (mm/dd)"
+            type="text"
+            value = {dueDate}
+            onChange={(e) => setDueDate(e.target.value)}
+            />
+
+          <br></br>
+          <br></br>
+
+          <Button type="ghost" onClick={addItem} shape="round" style={{backgroundColor: "#ffd6e7"}}> add item </Button>
+
+          <br></br>
+          <br></br>
+
+          <Space size = 'small' direction='horizontal'>
+            <SmileTwoTone twoToneColor="#fff566" style={{fontSize: '18px'}}/>
+            <ScheduleTwoTone twoToneColor="#b7eb8f" style={{fontSize: '18px'}}/>
+            <ShoppingTwoTone twoToneColor="#d3adf7" style={{fontSize: '18px'}}/>
+            <HeartTwoTone twoToneColor="#ffadd2" style={{fontSize: '18px'}}/>
+          </Space>
+          <br></br>
+          <br></br>
+
+        </div>
+      </Sider>
+
+      <Content style={{padding: '0 50px', backgroundColor: "#ffd6e7"}}>
+        <div style={{
+          display:"flex",
+          flexDirection:"row",
+          flexWrap: 'wrap',
+          
+        }}>
+          {tasks.map((todo) => <ToDoItem task={todo} />)}
+        </div>
+      </Content>
+    </Layout>
+
+    <Footer style={{
+      alignItems: "center",
       textAlign: "center",
       display: "flex",
-      flexDirection: "column"
-    }}
-    >
-    <title>To Do List</title>
-    <h1>Please Do These Things</h1>
+      flexDirection: "row", 
+      justifyContent: "center",
+      backgroundColor: "#d3adf7"
       
-    <label for="taskName">Name of Task:</label>
-    <input 
-      type="text" 
-      value = {taskName}
-      onChange={(e) => setName(e.target.value)}
-      />
-    <br></br>
+    }}>
 
-    <label for="taskDescription">Description:</label>
-    <input 
-      type="text"
-      value = {description}
-      onChange={(e) => setDescription(e.target.value)}
-      />
-    <br></br>
+      <Space size ='large' direction ='horizontal'>
+        <Button type = "default" onClick={orderDate} shape="round"> order by date </Button>
+      
+        <Button type = "default" onClick={orderName} shape="round"> order by name </Button>
+ 
+        <Button type="default" onClick={clearList} shape="round" style ={{backgroundColor:"#ffa39e"}}> clear list </Button>
+      </Space>
 
-    <label for="taskDueDate">Due Date:</label>
-    <input 
-      type="text"
-      value = {dueDate}
-      onChange={(e) => setDueDate(e.target.value)}
-      />
-    <br></br>
+    </Footer>
+    </Layout>
 
-    <button onClick={addItem}> Add it!</button>
-    <br></br>
-
-    {tasks.map((todo) => <ToDoItem task={todo} />)}
-    </div>
+  
   );
 };
 
